7eac2e00077e5e8cde7e5598522f4159ddd07b92
MrGithub2021 <77593158+MrGithub2021@users.noreply.github.com>
2021-03-23 13:00:09 -0400

    Update Square.rb

--- Ruby/Square.rb
+++ Ruby/Square.rb
@@ -1,15 +1,12 @@
 BEGIN{
     puts "Starting....."
 }
-class Sample
    def square(number)
       print "The square of #{number} is #{number * number}. \n"
   end
-end
 # Now using above class to create objects
 for i in 1..10
-object = Sample. new
-object.square(i)
+square(i)
 end
 END{
     puts "Program exited."
